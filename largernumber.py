e11d462c4137259917a2f6f13b807ad6b5b1754d
Ruzzel <123532716+rzzel@users.noreply.github.com>
2023-09-17 22:28:35 +0800

    Commit

---
+++ largernumber.py
@@ -0,0 +1,7 @@
+numa = int(input("Enter first number: "))
+numb = int(input("Enter second number: "))
+
+if numa > numb: larger_num = numa
+else: larger_num = numb
+    
+print(larger_num, " is the larger number.")
---
+++ strcompare.py
@@ -0,0 +1,11 @@
+ab = "Spathiphyllum"
+bc = "spathiphyllum"
+
+cd = input("ENter Spathiphyllum: ")
+
+if cd == ab:
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif cd == bc:
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not " , cd , "!")
