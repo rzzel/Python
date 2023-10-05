99d7ee74a2a5a031707bdad04030a7ed014d635f
Ruzzel <123532716+rzzel@users.noreply.github.com>
2023-10-05 23:01:13 +0800

    Add files via upload

---
+++ 2.0.15  LAB  Variables.js
@@ -0,0 +1,16 @@
+// Declare variables
+let maxwellWrightName = "Maxwell Wright";
+let maxwellWrightPhone = "(0191) 719 6495";
+let maxwellWrightEmail = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+let rajaVillarrealName = "Raja Villarreal";
+let rajaVillarrealPhone = "0866 398 2895";
+let rajaVillarrealEmail = "posuere.vulputate@sed.com";
+
+let helenRichardsName = "Helen Richards";
+let helenRichardsPhone = "0800 1111";
+let helenRichardsEmail = "libero@convallis.edu";
+
+// Display information about the first and last contact
+console.log(`${maxwellWrightName}/${maxwellWrightPhone}/${maxwellWrightEmail}`);
+console.log(`${helenRichardsName}/${helenRichardsPhone}/${helenRichardsEmail}`);
